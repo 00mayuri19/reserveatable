c7edf19bf1bd894d5a07630da7a3a9f476b91d86
00mayuri19 <164994767+00mayuri19@users.noreply.github.com>
2024-07-15 14:22:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Reserve from "./Reserve";
 import './App.css';
-
+import React from "react";
+import Handleclick from "./Handleclick";
 function App() {
-  return (
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar />
+     <Home />
+    <Reserve /> 
+    <Handleclick />
     </div>
   );
 }
