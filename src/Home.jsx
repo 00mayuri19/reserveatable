fa82eb52806af1649552ee0eee9ef704e9930e33
00mayuri19 <164994767+00mayuri19@users.noreply.github.com>
2024-07-15 14:23:15 +0530

    Create Home.jsx

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+const Home=()=>{
+    return(
+        <div className="main">
+            <h1>Special offer </h1>
+            <h2>Buy 1 get 1 FREEE on all Beverages</h2>
+        <div className="button">
+            <h3><a href="https://www.littlelemon.ie/#food">Book Now</a></h3>
+        </div>
+        </div>
+    )
+}
+export default Home;
